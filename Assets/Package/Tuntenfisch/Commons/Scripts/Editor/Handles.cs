79c56dde2007ab2067d3fce20fdaf72c680cbc52
Constantin Blessing <coblit01@hs-esslingen.de>
2022-03-08 18:35:48 +0100

    Handles.ColoredLabel uses black text color again.

--- Assets/Package/Tuntenfisch/Commons/Scripts/Editor/Handles.cs
+++ Assets/Package/Tuntenfisch/Commons/Scripts/Editor/Handles.cs
@@ -21,7 +21,7 @@ namespace Tuntenfisch.Commons.Editor
             float2 backgroundSize = new float2(1.1f, 1.25f) * contentSize;
             UnityEditor.Handles.BeginGUI();
             GUI.DrawTexture(new Rect(guiPoint.xy - 0.5f * backgroundSize, backgroundSize), Texture2D.whiteTexture, ScaleMode.StretchToFill, true, 0, color, 0.0f, 10.0f);
-            GUI.Label(new Rect(guiPoint.xy - 0.5f * contentSize, contentSize), content, EditorGUIStyles.CenteredLabelStyle);
+            GUI.Label(new Rect(guiPoint.xy - 0.5f * contentSize, contentSize), content, EditorGUIStyles.CenteredBlackLabelStyle);
             UnityEditor.Handles.EndGUI();
         }
         #endregion
